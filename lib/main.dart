44af98aa78ccf7aa41c31dd8645077d1272cff6a
ElderGiovani <elder.dev@hotmail.com>
2024-04-08 17:55:19 -0300

    despesas_pessoais _atualização

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:despesas_pessoais/models/transaction.dart';
-
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 main() {
   runApp(const ExpensesApp());
@@ -39,27 +39,71 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 255, 44, 7),
+        backgroundColor: const Color.fromARGB(255, 255, 176, 7),
         title: const Text('Despesas pessoais'),
       ),
       body: Column(
         mainAxisAlignment:
-            MainAxisAlignment.spaceAround, //spaceAround:deixa espço ao redor//
+            MainAxisAlignment.spaceAround, //spaceAround:deixa espaço ao redor//
         //eixo principal"vertical"
         crossAxisAlignment: CrossAxisAlignment.stretch, // stretch:estica
         //eixo secundario "horizontal"
         children: <Widget>[
           const SizedBox(
+            // SizedBox mais para espaçamento
             child: Card(
-              color: Color.fromARGB(255, 18, 196, 228),
+              color: Color.fromARGB(255, 185, 10, 216),
               elevation: 5,
               child: Text('Gráfico'),
             ),
           ),
           Column(
             children: _transactions.map((tr) {
+              //mapeamento entre objetos nao visuais convertendo em elementos visuais usando  a função "Map"
               return Card(
-                child: Text(tr.title),
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      // Usa "Container" quando precisa mais de duas propriedades. EX : margin,altuira ,largura,borda, cor
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 15, vertical: 10),
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.purple, width: 2)),
+                      padding: const EdgeInsets.all(10),
+                      // conteudo > padding > border > margin //
+                      child: Text(
+                        'R\$ ${tr.value.toStringAsFixed(2)}', //toStringAsFixed casas decimais
+                        style: const TextStyle(
+                          //elemento para estilizar o bloco
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple,
+                        ),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          tr.title,
+                          style: const TextStyle(
+                            //elemento para estilizar o texto
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          DateFormat("d MMM y").format(tr.date),
+                          style: const TextStyle(
+                            //elemento para estilizar a data
+                            fontSize: 13,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               );
             }).toList(),
           )
